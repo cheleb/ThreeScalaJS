ea3dd4db5d5270d74f5e83060c271155752d76d4
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-05-13 21:48:48 +0200

    Add AmbientLight class and companion object for global illumination

---
+++ modules/three/src/main/scala/THREE/lights/AmbientLight.scala
@@ -0,0 +1,33 @@
+package THREE
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+
+/**
+ * This light globally illuminates all objects in the scene equally.
+ * It cannot be used to cast shadows as it does not have a direction.
+ *
+ * ```scala
+ * val light = new AmbientLight(0x404040) // soft white light
+ * scene.add(light)
+ * ```
+ */
+@js.native
+@JSImport("three", "AmbientLight")
+class AmbientLight(
+  color: Int | String | Color = 0xffffff,
+  intensity: Double = 1.0
+) extends Light {
+
+  /**
+   * Read-only flag to check if a given object is of type AmbientLight.
+   */
+  val isAmbientLight: Boolean = js.native
+}
+
+/**
+ * Companion object for AmbientLight with factory method
+ */
+@js.native
+@JSImport("three", "AmbientLight")
+object AmbientLight extends js.Object
