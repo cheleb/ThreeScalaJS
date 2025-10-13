97382ede0774b6d689311bcdc8ad7140ab067623
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-10-13 19:07:01 +0200

    Refactor Header component by cleaning up code and updating profile avatar image

--- example/client/src/main/scala/dev/cheleb/scalajswebgl/app/Header.scala
+++ example/client/src/main/scala/dev/cheleb/scalajswebgl/app/Header.scala
@@ -5,8 +5,8 @@ import be.doeraene.webcomponents.ui5.configkeys.*
 import com.raquo.laminar.api.L.*
 
 object Header:
-  private val openPopoverBus = new EventBus[Boolean]
-  private val profileId      = "profileId"
+
+  private val profileId = "profileId"
 
   def apply(): HtmlElement =
     div(
@@ -15,12 +15,13 @@ object Header:
           Icon(_.name := IconName.home, cls := "pad-10"),
           href := Router.uiRoute("demo")
         ),
-        _.primaryTitle       := "ScalaJs WebGL",
-        _.secondaryTitle     := "ScalaJs WebGL bindings and utilities",
-        _.notificationsCount := "2+",
-        _.showNotifications  := true,
-        _.showCoPilot        := true,
-        _.slots.profile      := Avatar(idAttr := profileId, img(src := "img/questionmark.jpg")),
-        _.events.onProfileClick.mapTo(true) --> openPopoverBus
+        _.primaryTitle   := "ScalaJs WebGL",
+        _.secondaryTitle := "ScalaJs WebGL bindings and utilities",
+        _.showCoPilot    := true,
+        _.slots.profile  := Avatar(
+          idAttr := profileId,
+          img(src := "img/avatars/ono.png")
+        ),
+        _.events.onProfileClick.mapTo("https://github.com/cheleb/ThreeScalaJS/") --> Router.writer
       )
     )
